d3611e426c134cb2ec39155b197c08445d9c34c3
Juhyeon Yu <a01068079952@gmail.com>
2024-10-30 08:45:42 -0700

    Changed errors regarding login.html and its authentication

---
+++ scripts/firebaseAPI_BBY02.js
@@ -0,0 +1,14 @@
+//----------------------------------------
+//  Your web app's Firebase configuration
+//----------------------------------------
+var firebaseConfig = {
+    apiKey: "AIzaSyABRbMxdgLe4DsZx3-Zo-iqVnW3eUGPGIM",
+    authDomain: "kidsconnect-6259e.firebaseapp.com",
+    projectId: "kidsconnect-6259e",
+    storageBucket: "kidsconnect-6259e.appspot.com",
+    messagingSenderId: "313986570986",
+    appId: "1:313986570986:web:0b2e75a32df3dc251b1593"
+};
+
+const app = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
