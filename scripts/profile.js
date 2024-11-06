92e1982961d7bbda7bbf43c986a9449a0923a641
Juhyeon Yu <a01068079952@gmail.com>
2024-11-06 09:45:15 -0800

    Streamlined how nav and footer works by creating separate folder with html codes in it, then linking it>

--- scripts/profile.js
+++ scripts/profile.js
@@ -13,15 +13,11 @@ function populateUserInfo() {
                 .then(userDoc => {
                     //get the data fields of the user
                     let userName = userDoc.data().name;
-                    let userSchool = userDoc.data().school;
                     let userCity = userDoc.data().city;
 
                     //if the data fields are not empty, then write them in to the form.
                     if (userName != null) {
                         document.getElementById("nameInput").value = userName;
-                    }
-                    if (userSchool != null) {
-                        document.getElementById("schoolInput").value = userSchool;
                     }
                     if (userCity != null) {
                         document.getElementById("cityInput").value = userCity;
@@ -41,12 +37,10 @@ function editUserInfo() {
 
 function saveUserInfo() {
     userName = document.getElementById('nameInput').value;       //get the value of the field with id="nameInput"
-    userSchool = document.getElementById('schoolInput').value;     //get the value of the field with id="schoolInput"
     userCity = document.getElementById('cityInput').value;       //get the value of the field with id="cityInput"
 
     currentUser.update({
         name: userName,
-        school: userSchool,
         city: userCity
     })
         .then(() => {
