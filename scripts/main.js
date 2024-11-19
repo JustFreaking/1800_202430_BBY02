e9051e0f8ba9926aae1f6834d5b4b77812267570
Gabriel Wong <gabriel2446@gmail.com>
2024-11-19 15:55:49 -0800

    Added general css changes to make the website look better

--- scripts/main.js
+++ scripts/main.js
@@ -53,17 +53,16 @@ function writeEvents() {
 // Input parameter is a string representing the collection we are reading from
 //------------------------------------------------------------------------------
 function displayCardsDynamically(collection) {
-    let cardTemplate = document.getElementById("eventsCardTemplate"); // Retrieve the HTML element with the ID "hikeCardTemplate" and store it in the cardTemplate variable. 
-
-    db.collection(collection).get()   //the collection called "hikes"
+    let cardTemplate = document.getElementById("eventsCardTemplate"); 
+    db.collection(collection).get()  
         .then(allEvents => {
-            var i = 1;  //Optional: if you want to have a unique ID for each event
-            allEvents.forEach(doc => { //iterate thru each doc
-                var title = doc.data().title;       // get value of the "name" key
-                var description = doc.data().description;  // get value of the "details" key
-                var time = doc.data().time; //gets the length field
+            var i = 1; 
+            allEvents.forEach(doc => { 
+                var title = doc.data().title;     
+                var description = doc.data().description; 
+                var time = doc.data().time; 
                 var docID = doc.id;
-                let newcard = cardTemplate.content.cloneNode(true); // Clone the HTML template to create a new card (newcard) that will be filled with Firestore data.
+                let newcard = cardTemplate.content.cloneNode(true); 
 
                 //update title and text and image
                 newcard.querySelector('.card-title').innerHTML = title;
