88618019f30178353c37bddbe6694785eca1b378
Parvaneh-S <psaberijosheghani@my.bcit.ca>
2024-12-04 12:27:33 -0800

    Format index.js file and add comments to it

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
+
+/**
+ * We have used node to run our application from the server side, 
+ * this is the node js file that we run it through the command line. 
+ */
+
+
+// Sets up Express to serve static files from specified directories 
+// for different content types (HTML, JS, CSS, images, and text).
 const express = require('express');
 const path = require('path');
 const app = express();
-
 app.use("/html", express.static("./app/html"));
 app.use("/js", express.static("./public/scripts"));
 app.use("/css", express.static("./public/styles"));
